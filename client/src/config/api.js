b0b799eaf93674683f8cfb8a7836810b73acbafe
lorryaze <lorrany90@gmail.com>
2021-05-03 20:26:43 -0300

    Add api route

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const DEV_URL = "http://localhost:8000";
-const PROD_URL = "https://client-dasdasd.herokuapp.com/#/";
+const PROD_URL = "https://api-trabalho-indivudual.herokuapp.com";
 const BASE_URL = process.env.NODE_ENV !== "production" ? DEV_URL : PROD_URL;
 
 export const api = axios.create({
